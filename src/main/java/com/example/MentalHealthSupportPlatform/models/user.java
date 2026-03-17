2a81a9cbe4f1153f1a8f06bf5c02cf1986dcecd9
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-03-17 14:52:11 +0530

    user model add

---
+++ src/main/java/com/example/MentalHealthSupportPlatform/models/user.java
@@ -0,0 +1,27 @@
+package com.example.MentalHealthSupportPlatform.models;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+
+public class user {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String phone;
+
+    private String role;
+}
