88ee2de67d0422bf7eacd86ac5450d9d859ffdb9
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-03-17 15:08:36 +0530

    user registeration or singup moodule completed

---
+++ src/main/java/com/example/MentalHealthSupportPlatform/services/AuthService.java
@@ -0,0 +1,40 @@
+package com.example.MentalHealthSupportPlatform.services;
+
+import com.example.MentalHealthSupportPlatform.dto.UserRegisterDTO;
+import com.example.MentalHealthSupportPlatform.models.User;
+import com.example.MentalHealthSupportPlatform.repositories.CounselorRepository;
+import com.example.MentalHealthSupportPlatform.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+        @Autowired
+        private UserRepository userRepository;
+
+        @Autowired
+        private CounselorRepository counselorRepository;
+
+        @Autowired
+        private BCryptPasswordEncoder passwordEncoder;
+
+        // 🔹 USER REGISTER
+        public User registerUser(UserRegisterDTO dto) {
+
+            if (userRepository.findByEmail(dto.getEmail()).isPresent()) {
+                throw new RuntimeException("Email already exists");
+            }
+
+            User user = new User();
+            user.setName(dto.getName());
+            user.setEmail(dto.getEmail());
+            user.setPassword(passwordEncoder.encode(dto.getPassword()));
+            user.setPhone(dto.getPhone());
+            user.setRole("USER");
+
+            return userRepository.save(user);
+        }
+
+    }
