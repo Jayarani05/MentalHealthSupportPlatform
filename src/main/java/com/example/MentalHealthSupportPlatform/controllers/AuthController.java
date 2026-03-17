f66f2e407983eeeb775442064a307b1d770f4d48
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-03-17 15:13:54 +0530

    user registration for auth controller

--- src/main/java/com/example/MentalHealthSupportPlatform/controllers/AuthController.java
+++ src/main/java/com/example/MentalHealthSupportPlatform/controllers/AuthController.java
@@ -1,5 +1,20 @@
 package com.example.MentalHealthSupportPlatform.controllers;
 
+import com.example.MentalHealthSupportPlatform.dto.UserRegisterDTO;
+import com.example.MentalHealthSupportPlatform.services.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
 
+        @Autowired
+        private AuthService authService;
+
+        @PostMapping("/register/user")
+        public String registerUser(@RequestBody UserRegisterDTO dto) {
+            authService.registerUser(dto);
+            return "User registered successfully";
+        }
 }
