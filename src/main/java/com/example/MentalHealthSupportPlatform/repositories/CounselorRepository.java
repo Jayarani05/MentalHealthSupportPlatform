ef640bc5f160e4bcf34215129914d0df39f86dd7
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-03-17 15:05:13 +0530

    counseller repo and security configuration updated

---
+++ src/main/java/com/example/MentalHealthSupportPlatform/repositories/CounselorRepository.java
@@ -0,0 +1,7 @@
+package com.example.MentalHealthSupportPlatform.repositories;
+import com.example.MentalHealthSupportPlatform.models.Counselor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CounselorRepository extends JpaRepository<Counselor, Long> {
+
+}
