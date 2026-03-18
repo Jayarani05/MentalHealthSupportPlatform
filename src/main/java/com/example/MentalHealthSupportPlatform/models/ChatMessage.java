ba2e44e17b21e198132cc337cad65a4e273ddd89
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-03-18 10:40:37 +0530

    entity for chat message

---
+++ src/main/java/com/example/MentalHealthSupportPlatform/models/ChatMessage.java
@@ -0,0 +1,28 @@
+package com.example.MentalHealthSupportPlatform.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "chat_messages")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String sender;
+
+    private String receiver;
+
+    private String message;
+
+    private Long sessionId;
+
+    private LocalDateTime timestamp;
+}
