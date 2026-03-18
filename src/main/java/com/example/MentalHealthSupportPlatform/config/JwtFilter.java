61859e5af62088f5d062d9b560b4bdc8e0b268f1
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-03-18 09:27:43 +0530

    JWT FILTERATION MODULE

---
+++ src/main/java/com/example/MentalHealthSupportPlatform/config/JwtFilter.java
@@ -0,0 +1,43 @@
+package com.example.MentalHealthSupportPlatform.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+
+            String token = authHeader.substring(7);
+
+            try {
+                String email = jwtUtil.extractEmail(token);
+                System.out.println("Valid user: " + email);
+            } catch (Exception e) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+        } else {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
